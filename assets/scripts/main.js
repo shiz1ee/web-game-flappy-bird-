51ab451a938a05c279178ba7e9355f95fe7bb433
Sainath <sainathshinde200815@gmail.com>
2026-09-04 22:33:49 +0530

    added touch controls

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,12 +7,26 @@ class Game {
         this.baseHeight = 720;
         this.ratio = this.height / this.baseHeight;
         this.player = new Player(this);
+        this.gravity;
 
         this.resize(window.innerWidth, window.innerHeight); 
 
         window.addEventListener('resize', e =>{
             console.log(e);
             this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
+        });
+        // mouse cons
+        this.canvas.addEventListener('mousedown', e => {
+            this.player.flapp();
+        });
+        // keyboard cons
+        window.addEventListener('keydown', e => {
+            console.log(e.key);
+            if (e.key === ' ' || e.key === 'Enter') this.player.flapp();
+        });
+        //touchy touchy cons
+        this.canvas.addEventListener('touchstart', e => {
+            console.log(e);
         });
 
     }
@@ -24,8 +38,8 @@ class Game {
             this.height = this.canvas.height;
             this.ratio = this.height / this.baseHeight;
 
+            this.gravity = 0.155* this.ratio;
             this.player.resize();
-            console.log(this.height, this.baseHeight, this.ratio);
 
     }
     render(){
