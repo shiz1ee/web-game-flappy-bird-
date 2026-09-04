d32ae317b80cc5c33e527ea56f631392bd243e4c
Sainath <sainathshinde200815@gmail.com>
2026-09-04 22:28:34 +0530

    added mouse controls

---
+++ assets/scripts/main.js
@@ -0,0 +1,53 @@
+class Game {
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.ctx = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.baseHeight = 720;
+        this.ratio = this.height / this.baseHeight;
+        this.player = new Player(this);
+
+        this.resize(window.innerWidth, window.innerHeight); 
+
+        window.addEventListener('resize', e =>{
+            console.log(e);
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
+        });
+
+    }
+    resize(width, height){
+            this.canvas.width = width;
+            this.canvas.height = height;
+            this.ctx.fillStyle = 'purple';
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.ratio = this.height / this.baseHeight;
+
+            this.player.resize();
+            console.log(this.height, this.baseHeight, this.ratio);
+
+    }
+    render(){
+        this.player.update();
+        this.player.draw();
+    }
+}
+
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 720;
+    canvas.height = 720;
+
+    const game = new Game(canvas, ctx);
+    game.render();
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render();
+        requestAnimationFrame(animate);
+
+    }
+    requestAnimationFrame(animate);
+});
--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -1,10 +1,14 @@
 class Player {
     constructor(game) {
         this.game = game;
-        this.x = 50;
-        this.y = 60;
-        this.width = 200;
-        this.height = 200;
+        this.x = 20;
+        this.y;
+        this.spriteWidth = 200;
+        this.spriteHeight = 200;
+        this.width;
+        this.height;
+        this.speedY = -5;
+        this.flapspeed;
 
     }
     draw(){
@@ -12,6 +16,32 @@ class Player {
 
     }
     update(){
-        this.x++;
+        this.y += this.speedY;
+        if (!this.istouchingbottom()) {
+            this.speedY += this.game.gravity;
+        }
+
+        if (this.istouchingbottom()) {
+            this.y = this.game.height - this.height;
+        }
+    }
+    resize(){
+        this.width = this.spriteWidth * this.game.ratio;
+        this.height = this.spriteHeight * this.game.ratio;
+        this.y = this.game.height * 0.5 - this.height * 0.5;
+        this.speedY = -8 * this.game.ratio;
+        this.flapspeed = 5 * this.game.ratio;
+
+    }
+    istouchingtop(){
+        return this.y <= 0;
+    }
+    istouchingbottom(){
+        return this.y >= this.game.height - this.height;
+    }
+    flapp(){
+        if (!this.istouchingtop()) {
+            this.speedY = -this.flapspeed;
+        }
     }
 }
